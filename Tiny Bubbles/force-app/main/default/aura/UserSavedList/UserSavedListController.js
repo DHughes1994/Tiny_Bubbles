5aaad9adfeaefbc561829e5043a3f2e465794d83
Dillon Hughes <hughes.dillon1994@gmail.com>
2020-06-25 19:36:20 -0400

    The real last thing that was done for ben

--- Tiny Bubbles/force-app/main/default/aura/UserSavedList/UserSavedListController.js
+++ Tiny Bubbles/force-app/main/default/aura/UserSavedList/UserSavedListController.js
@@ -1,5 +1,14 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doinit : function(component, event, helper) {
+        component.set('v.SavedList',[
+            {label: 'Name', fieldName: 'Name', type: 'String'},
+            {label: 'Type', fieldName: 'Type__c', type: 'String'},
+            {label: 'Address', fieldName: 'Street_Address__c', type: 'String'},
+            {label: 'City', fieldName: 'City__c', type: 'String'},
+            {label: 'State', fieldName: 'State__c', type: 'String'},
+            {label: 'Zip Code', fieldName: 'Zip_Code__c', type: 'String'},
+            {label: 'Phone Number', fieldName: 'Phone__c', type: 'Number'},
+            {label: 'Website', fieldName: 'Website__c', type: 'URL', typeAttributes: {target: '_self'}}
+        ]);
     }
 })
--- Tiny Bubbles/force-app/main/default/aura/UserSavedList/UserSavedListHelper.js
+++ Tiny Bubbles/force-app/main/default/aura/UserSavedList/UserSavedListHelper.js
@@ -1,5 +1,15 @@
 ({
-    helperMethod : function() {
-
+    getData : function(cmp) {
+        var action = cmp.get('c.getBreweries');
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set('v.Brewery', response.getReturnValue());
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                console.error(errors);
+            }
+        }));
+        $A.enqueueAction(action);
     }
 })
