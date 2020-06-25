6367cd055cf42d833d76436409e5ed4d1ed9e2b5
Dillon Hughes <hughes.dillon1994@gmail.com>
2020-06-25 11:17:21 -0400

    Succeffuly displaying hard coded search data

--- Tiny Bubbles/force-app/main/default/aura/BreweryCityList/BreweryCityListController.js
+++ Tiny Bubbles/force-app/main/default/aura/BreweryCityList/BreweryCityListController.js
@@ -1,5 +1,28 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
 
+        component.set('v.BreweryInfo',[
+            {label: 'Name', fieldName: 'Name', type: 'String'},
+            {label: 'Type', fieldName: 'Type', type: 'String'},
+            {label: 'Address', fieldName: 'Street', type: 'String'},
+            {label: 'City', fieldName: 'City', type: 'String'},
+            {label: 'State', fieldName: 'State', type: 'String'},
+            {label: 'Zip Code', fieldName: 'ZipCode', type: 'String'},
+            {label: 'Phone Number', fieldName: 'Phone', type: 'String'},
+            {label: 'Website', fieldName: 'Website', type: 'String'}
+        ]);
+
+        var action = component.get("c.getBreweriesbyCity");
+        action.setParams({'City':'Atlanta'});
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.Brewery", response.getReturnValue());
+                console.log(response.getReturnValue());
+            } else {
+                console.log("NoGo");
+            }
+        });
+        $A.enqueueAction(action);
     }
 })
